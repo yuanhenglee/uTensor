090bb57da89a5740df823eaa0764b82755b14265
yuanhenglee <yuanhenglee2000@gmail.com>
2024-06-09 19:38:22 +0800

    fix: conv padding

--- TESTS/pytest/conftest.py
+++ TESTS/pytest/conftest.py
@@ -25,3 +25,20 @@ def random_broadcastable_shapes():
             pairs.append((shape_a, shape_a[-randint(1, 3):]))
     
     return pairs
+
+@pytest.fixture(scope="function")
+def random_conv_input():
+    inputs = []
+    rand_max = 10
+
+    for _ in range(100):
+        B, H, W, Cin = randint(1, rand_max), randint(1, rand_max), randint(1, rand_max), randint(1, rand_max)
+        kernelH, kernelW, Cout = randint(1, H), randint(1, W), randint(1, rand_max)
+        bias = np.random.rand(Cout).astype(np.float32)
+        strides = [1, randint(1, 3), randint(1, 3), 1]
+        padding = choice(["VALID", "SAME"])
+        input_tensor = np.random.rand(B, H, W, Cin).astype(np.float32)
+        filter_tensor = np.random.rand(kernelH, kernelW, Cin, Cout).astype(np.float32)
+        inputs.append((input_tensor, filter_tensor, bias, strides, padding))
+
+    return inputs
